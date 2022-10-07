b8047821f4a3131c1de0297c1727521463b15532
alexehouston <alexehouston@gmail.com>
2022-10-07 18:26:56 -0500

    add animations

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 // constants
 const CHAR_LOOKUP = {
-    mercury: '/img/sailor-mercury.png',
-    venus: '/img/sailor-venus.png',
-    moon: '/img/sailor-moon.png',
-    chibi: '/img/sailor-chibi-moon.png',
-    mars: '/img/sailor-mars.png',
-    jupiter: '/img/sailor-jupiter.png',
-    saturn: '/img/sailor-saturn.png',
-    neptune: '/img/sailor-neptune.png',
-    uranus: '/img/sailor-uranus.png',
-    pluto: '/img/sailor-pluto.png',
+    mercury: 'img/sailor-mercury.png',
+    venus: 'img/sailor-venus.png',
+    moon: 'img/sailor-moon.png',
+    chibi: 'img/sailor-chibi-moon.png',
+    mars: 'img/sailor-mars.png',
+    jupiter: 'img/sailor-jupiter.png',
+    saturn: 'img/sailor-saturn.png',
+    neptune: 'img/sailor-neptune.png',
+    uranus: 'img/sailor-uranus.png',
+    pluto: 'img/sailor-pluto.png',
 }
 
 // variables
 let firstCard;
 let secondCard;
-let cardFlipped;
-let lockBoard;
+let cardFlipped = false;
+let lockedBoard = false;
 
 // cached elements
 const cards = document.getElementById('memory-card');
@@ -35,20 +35,41 @@ function init() {
 
 function flipCard() {
     // if board is not locked -> allow cards to flip
+    if (lockedBoard) return;
+    if (this === firstCard) return;
+        this.classList.add('flip');
     // if choosing first or second card -> allow card to flip
-    // if choosing second card -> check for match
+    if (!cardFlipped) {
+        cardFlipped = true;
+        firstCard = this;
+    } else {
+    // check for match after selecting second card
+        cardFlipped = false;
+        secondCard = this;
+        checkMatch();
+    }
 }
 
 function checkMatch() {
-    // if first & second card match -> keep cards facing up -> continue game
-    // otherwise -> flip cards back to normal position
+    // if first & second card match -> keep cards facing up
+    if (firstCard.dataset === secondCard.dataset) {
+        freezeCards();
+    } else {
+        // otherwise -> flip cards back to normal position
+        resetCards();
+    }
+}
+
+function freezeCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
 }
 
 function matchedCards() {
     // make matched cards unclickable/unflippable -> reset the remainder of the board
 }
 
-function unflipCards() {
+function resetCards() {
     // player clicks two cards -> cards unflip after a few seconds
 }
 
